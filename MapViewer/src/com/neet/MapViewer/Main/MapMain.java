835389839330d1e91a554363fe17af4467e305e0
krishtan <58432812+krishtan@users.noreply.github.com>
2019-12-08 16:24:56 +0800

    Updated MapMain Class
    Added two methods and then added the main method to lauch the map viewer.

--- MapViewer/src/com/neet/MapViewer/Main/MapMain.java
+++ MapViewer/src/com/neet/MapViewer/Main/MapMain.java
@@ -55,4 +55,36 @@ public class MapMain extends Application {
             e.printStackTrace();
         }
     }
+    private void setFadeAnimation() {
+    	FadeTransition ft = new FadeTransition(Duration.millis(5000), rootLayout);
+    	ft.setFromValue(0);
+    	ft.setToValue(1.0);
+    	ft.setCycleCount(1);
+    	ft.setAutoReverse(true);
+    	ft.play();
+    }
+    
+    public void showMapOverview() {
+    	tileMapViewer = new TileMapViewer();
+    	tileMapViewer.loadMapFile("/Maps/testmap.map");
+    	tileMapViewer.loadImagesFiles("/Tilesets/testtileset.gif", "/Sprites/items.gif");
+	    
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MapMain.class.getResource("/com/neet/MapViewer/View/MapOverview.fxml"));
+			tileOverview = (TilePane) loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		tileOverview.setPrefColumns(tileMapViewer.numCols);
+		tileOverview.setPrefRows(tileMapViewer.numRows);
+		tileMapViewer.initialiseCanvas();
+		tileOverview.getChildren().add(tileMapViewer.currentCanvas);
+		rootLayout.setCenter(tileOverview);
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
