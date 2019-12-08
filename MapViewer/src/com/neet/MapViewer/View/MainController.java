acda8c9d5df3ea9d547f712e521e3ba6f45e06b9
mehulbhuradia <53063507+mehulbhuradia@users.noreply.github.com>
2019-12-08 16:25:33 +0800

    Added methods
    Added a few methods -handleKeyAction(KeyEvent event), handleSetPosition(KeyEvent event), exit(), zoomInFromMenu(), zoomOutFromMenu(), updateCursorPosition().

--- MapViewer/src/com/neet/MapViewer/View/MainController.java
+++ MapViewer/src/com/neet/MapViewer/View/MainController.java
@@ -26,4 +26,72 @@ public class MainController {
 	@FXML
 	private Label boatPosition;
 
+
+	@FXML
+	private void handleKeyAction(KeyEvent event) {
+		if (MapMain.tileMapViewer.cursorColor == false) {
+			information.setText("X: Axe, C: Boat");
+		}
+
+		if(event.getCode() == KeyCode.K) {
+			MapMain.tileMapViewer.zoomInImage();
+	    }
+	    else if (event.getCode() == KeyCode.J) {
+	    	MapMain.tileMapViewer.zoomOutImage();
+	    }
+	    else if (event.getCode() == KeyCode.W || event.getCode() == KeyCode.UP) {
+	    	MapMain.tileMapViewer.cursorUp();
+	    	updateCursorPosition();
+	    }
+	    else if (event.getCode() == KeyCode.S || event.getCode() == KeyCode.DOWN) {
+	    	MapMain.tileMapViewer.cursorDown();
+	    	updateCursorPosition();
+	    }
+	    else if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.LEFT) {
+	    	MapMain.tileMapViewer.cursorLeft();
+	    	updateCursorPosition();
+	    }
+	    else if (event.getCode() == KeyCode.D || event.getCode() == KeyCode.RIGHT) {
+	    	MapMain.tileMapViewer.cursorRight();
+	    	updateCursorPosition();
+	    }
+	    else if (event.getCode() == KeyCode.X) {
+	    	MapMain.tileMapViewer.turningOnCurorColor();
+	    }
+	    else if (event.getCode() == KeyCode.C) {
+	    	MapMain.tileMapViewer.turningOnCurorColor();
+	    }
+	    else if (event.getCode() == KeyCode.ENTER) {
+			MapMain.primaryStage.hide();
+			Game.main(null);
+	    }
+	}
+
+	@FXML
+	private void handleSetPosition(KeyEvent event) {
+		if (event.getCode() == KeyCode.X) 
+			MapMain.tileMapViewer.handleSetAxeRequest();
+		else if (event.getCode() == KeyCode.C) 
+			MapMain.tileMapViewer.handleSetBoatRequest();
+	}
+
+	@FXML
+	private void exit() {
+		Platform.setImplicitExit(true);
+		MapMain.primaryStage.hide();
+	}
+
+	@FXML
+	private void zoomInFromMenu() {
+		MapMain.tileMapViewer.zoomInImage();
+	}
+	
+	@FXML
+	private void zoomOutFromMenu() {
+		MapMain.tileMapViewer.zoomOutImage();
+	}
+	
+	private void updateCursorPosition() {
+		cursorPosition.setText("x: " + MapMain.tileMapViewer.cursor.cursorRows + ", y: " + MapMain.tileMapViewer.cursor.cursorCols);
+	}
 }
