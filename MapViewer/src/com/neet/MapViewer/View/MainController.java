710bab4d57c4e5c98eaabec8b98e4731fe77fe2e
mehulbhuradia <53063507+mehulbhuradia@users.noreply.github.com>
2019-12-08 15:57:23 +0800

    Created MainController.java
    Added the required imports and declared the variables i would use.

---
+++ MapViewer/src/com/neet/MapViewer/View/MainController.java
@@ -0,0 +1,29 @@
+package com.neet.MapViewer.View;
+
+import com.neet.DiamondHunter.Main.Game;
+import com.neet.MapViewer.Main.MapMain;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class MainController {
+
+	@FXML
+	private Label cursorPosition;
+	
+	@FXML
+	private Label information;
+
+	@FXML
+	private Label axePosition;
+
+	@FXML
+	private Label boatPosition;
+
+}
